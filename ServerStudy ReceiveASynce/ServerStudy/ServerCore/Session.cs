4d7a0a1e2b26786e0ad89a1d5d1a375d480066e5
ricethief0 <net050722@gmail.com>
2021-04-17 23:07:11 +0900

    Send Async로 변경 및 대량의 정보를List에 담아서 전송할수 있도록 변경

--- ServerStudy ReceiveASynce/ServerStudy/ServerCore/Session.cs
+++ ServerStudy ReceiveASynce/ServerStudy/ServerCore/Session.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -9,19 +10,35 @@ namespace ServerCore
     {
         Socket m_socket;
         int mutex = 0;
-
+        Queue<byte[]> sendQue = new Queue<byte[]>();
+        //bool m_isPending = false;
+        List<ArraySegment<byte>> m_penddingList = new List<ArraySegment<byte>>();
+        SocketAsyncEventArgs m_sendArgs = new SocketAsyncEventArgs();
+        SocketAsyncEventArgs m_recArgs = new SocketAsyncEventArgs();
+        object m_lock = new object();
         public void Start(Socket socket)
         {
             m_socket = socket;
-            SocketAsyncEventArgs arg = new SocketAsyncEventArgs();
-            arg.Completed += new EventHandler<SocketAsyncEventArgs>(OnComplete);
 
-            arg.SetBuffer(new byte[1024], 0, 1024);
-            RegisterRecev(arg);
+           
+            m_recArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecevComplete);
+            m_recArgs.SetBuffer(new byte[1024], 0, 1024);
+            RegisterRecev();
+
+            m_sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompelte);
+
         }
         public void Send(byte[] buff)
         {
-            m_socket.Send(buff);
+            lock(m_lock)
+            {
+                sendQue.Enqueue(buff);
+                if (m_penddingList.Count == 0)
+                {
+                    RegisterSend();
+                }
+            }
+            
         }
 
         public void DisConnect()
@@ -33,39 +50,83 @@ namespace ServerCore
             m_socket.Close();
         }
         #region 네트워크통신
-        void RegisterRecev(SocketAsyncEventArgs arg)
+
+        void RegisterSend()
         {
-           bool pending =  m_socket.ReceiveAsync(arg);
+            while(sendQue.Count>0)
+            {
+                byte[] buff = sendQue.Dequeue();
+                m_penddingList.Add(new ArraySegment<byte>(buff, 0, buff.Length));
+            }
+            m_sendArgs.BufferList = m_penddingList;
+
+            bool pending = m_socket.SendAsync(m_sendArgs);
+            if (pending == false)
+            {
+                OnSendCompelte(null, m_sendArgs);
+            }
+        }
+
+        void OnSendCompelte(object obj, SocketAsyncEventArgs arg)
+        {
+            lock(m_lock)
+            {
+                if(arg.BytesTransferred>0 && SocketError.Success == arg.SocketError)
+                {
+                    try
+                    {
+                        m_sendArgs.BufferList = null;
+                        m_penddingList.Clear();
+                        if (sendQue.Count > 0)
+                        {
+                            RegisterSend();
+                        }
+                    }
+                    catch (Exception e )
+                    {
+
+                        Console.WriteLine($"Exception : {e}");
+                    }
+                }
+                else
+                {
+                    DisConnect();
+                }
+            }
+        }
+
+
+        void RegisterRecev()
+        {
+           bool pending =  m_socket.ReceiveAsync(m_recArgs);
             if(pending == false)
             {
-                OnComplete(null, arg);
+                OnRecevComplete(null, m_recArgs);
             }
-            
-
         }
 
        
-        void OnComplete(object sender, SocketAsyncEventArgs arg)
+        void OnRecevComplete(object sender, SocketAsyncEventArgs arg)
         {
-            try
+
+            if (arg.BytesTransferred > 0 && SocketError.Success == arg.SocketError)
             {
-                if (arg.BytesTransferred > 0 && SocketError.Success == arg.SocketError)
+                try
                 {
                     string recData = Encoding.UTF8.GetString(arg.Buffer, arg.Offset, arg.BytesTransferred);
 
                     Console.WriteLine($"[Client Data] : {recData}");
-                    RegisterRecev(arg);
+                    RegisterRecev();
                 }
-                else
+                catch (Exception ex)
                 {
-
+                    Console.WriteLine($"OnRecvComplete Fail : {ex}");
                 }
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine($"OnRecvComplete Fail : {ex}");
+                DisConnect();
             }
-            
         }
         #endregion
     }
